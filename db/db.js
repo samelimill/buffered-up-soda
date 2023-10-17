4b57017f8190f9f52cec7f76260eb2c380dc9369
samelimill <samelimill@gmail.com>
2023-10-17 11:11:47 -0700

    Started addition functions - addDept works!

--- db/db.js
+++ db/db.js
@@ -14,26 +14,25 @@ const viewStuff = (location) => {
     var queryString = ``;
     if (location === 'department'){
         queryString = 
-        `SELECT id AS 'ID', name AS 'Department'
+        `SELECT department.id AS 'ID', name AS 'Department'
         FROM department
         ORDER BY id ASC`;
     } else if (location === 'role'){
         queryString = 
-        `SELECT title AS 'Role', name AS 'Department', salary AS 'Salary'
+        `SELECT role.id AS 'ID', title AS 'Role', name AS 'Department', salary AS 'Salary'
         FROM role
         LEFT OUTER JOIN department ON role.department_id = department.id
         GROUP BY role.id
         ORDER BY title ASC`;
     } else if (location === 'employee'){
         queryString = 
-        `SELECT GROUP_CONCAT(DISTINCT last_name,', ', first_name) AS 'Name', title AS 'Title', name AS 'Department', salary AS 'Salary', manager_id AS 'Manager'
+        `SELECT employee.id AS 'ID', first_name as 'First Name', last_name as 'Last Name', title AS 'Title', name AS 'Department', salary AS 'Salary', manager_id AS 'Manager'
         FROM employee
-        LEFT OUTER JOIN role ON employee.role_id = role.id
-        LEFT OUTER JOIN department ON role.department_id = department.id
+        LEFT JOIN role ON employee.role_id = role.id
+        LEFT JOIN department ON role.department_id = department.id
         GROUP BY employee.id
         ORDER BY last_name ASC`;
     }
-    console.log(queryString);
     db.query(queryString, (err, res) => {
         if (err){
             console.log(err)
@@ -44,6 +43,26 @@ const viewStuff = (location) => {
     });
 };
 
+const addStuff = (stuff) => {
+    var queryString = '';
+    if (stuff.type === 'Department'){
+        queryString = `
+        INSERT INTO department (name)
+        VALUES ('${stuff.data}')`
+    } else if (stuff.type = 'Role'){
+        queryString = `INSERT INTO role (title, salary, department_id)
+        VALUES (${stuff.title},${stuff.salary},${stuff.department})`
+    }
+    db.query(queryString, (err, res) => {
+        if (err){
+            console.log(err)
+        } else {
+            console.log(`${stuff.type} added`)
+        }
+    });
+}
+
 module.exports = {
-    viewStuff
+    viewStuff,
+    addStuff,
 };
--- index.js
+++ index.js
@@ -14,41 +14,71 @@ app.use(express.json());
 
 const init = () => {
   cfonts.say('Employee|Manager', {font: 'simple3d'});
+  routePrompt();
+};
+
+const routePrompt = () => {
+  inquirer
+  .prompt([{
+      type: 'list',
+      message: 'What would you like to do?',
+      name: 'options',
+      choices: [
+          'View All Employees',
+          'Add an Employee',
+          'Update an Employee Role',
+          new inquirer.Separator(),
+          'View All Departments',
+          'Add a Department',
+          new inquirer.Separator(),
+          'View All Roles',
+          'Add a Role',
+          new inquirer.Separator(),
+      ]
+  }]).then(res => {
+    switch(res.options) {
+      case 'View All Departments':
+        return indexView('department');
+      case 'Add a Department':
+        
+        console.log('hey')
+        return addDept();
+      case 'View All Roles':
+        return indexView('role');
+      case 'Add a Role':
+        return addRole();
+      case 'View All Employees':
+        return indexView('employee');
+      case 'Add an Employee':
+        return addEmployee();
+      case 'Update an Employee Role':
+        return update();
+    }
+  })
+};
+
+const indexView = (location) => {
+  db.viewStuff(location);
+  routePrompt();
+}
+
+const addDept = () => {
   inquirer
     .prompt([{
-        type: 'list',
-        message: 'What would you like to do?',
-        name: 'options',
-        choices: [
-            'View All Departments',
-            'Add a Department',
-            new inquirer.Separator(),
-            'View All Roles',
-            'Add a Role',
-            new inquirer.Separator(),
-            'View All Employees',
-            'Add an Employee',
-            'Update an Employee Role',
-            new inquirer.Separator(),
-        ]
-    }]).then(res => {
-      switch(res.options) {
-        case 'View All Departments':
-          return db.viewStuff('department');
-        case 'View All Roles':
-          return db.viewStuff('role');
-        case 'View All Employees':
-          return db.viewStuff('employee');
-        case 'Add A Department':
-          return addDept();
-        case 'Add A Role':
-          return addRole();
-        case 'Add An Employee':
-          return addEmployee();
-        case 'Update An Employee Role':
-          return update();
-      }
+      type: 'input',
+      message: 'What is the name of the department you would like to add?',
+      name: 'departmentInput',
+    }])
+    .then(res => {
+      const addDeptData = {type:'Department',data:res.departmentInput}
+      db.addStuff(addDeptData);
     })
 };
 
+const addRole = () => {
+  inquirer
+    .prompt([
+      
+    ])
+}
 init();
